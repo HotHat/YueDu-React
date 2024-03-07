be7091dc14cbe488a5038b8604d29843848d562f
luyuh <774964107@qq.com>
2024-03-07 15:49:17 +0800

    more category page code

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -8,6 +8,13 @@ import Grid from '@mui/material/Unstable_Grid2';
 import {
 	useNavigate 
 } from "react-router-dom";
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -22,21 +29,42 @@ export default function CategoryPage() {
 
 	return (
 		<>
-		<Grid container spacing={0.5}>
-			<Grid xs={6}>
-				<Item>xs=6</Item>
+		<Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+						style={{marginRight: 0}}
+            sx={{ mr: 2 }}
+						onClick={() => {return navigate('/')}}
+          >
+            <ArrowBackIosIcon/>
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						分类
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    <Container maxWidth="sm">
+			<Grid container spacing={1}>
+				<Grid xs={6}>
+					<Item>xs=6</Item>
+				</Grid>
+				<Grid xs={6}>
+					<Item>xs=6</Item>
+				</Grid>
+				<Grid xs={6}>
+					<Item>xs=6</Item>
+				</Grid>
+				<Grid xs={6}>
+					<Item>xs=6</Item>
+				</Grid>
 			</Grid>
-			<Grid xs={6}>
-				<Item>xs=6</Item>
-			</Grid>
-			<Grid xs={6}>
-				<Item>xs=6</Item>
-			</Grid>
-			<Grid xs={6}>
-				<Item>xs=6</Item>
-			</Grid>
-		</Grid>
-		<Button onClick={() => {return navigate('/')}} >Click</Button>
+		</Container>
 		</>
 	)
 }
